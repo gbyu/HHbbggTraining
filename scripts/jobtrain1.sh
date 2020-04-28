013eecb8d8877bbcb63e51961bd2720c97f4bbc4
ivovtin <ivan.ovtin@cern.ch>
2020-04-28 09:04:06 +0200

    28042020

--- scripts/jobtrain1.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-cd /afs/cern.ch/user/i/ivovtin/HHggbb/CMSSW_10_2_13/src/
-#cmsenv
-eval `scramv1 runtime -sh`
-echo "CMSSW: "$CMSSW_BASE
-#Run your program
-cd /afs/cern.ch/user/i/ivovtin/HHggbb/HHbbggTraining/scripts/
-ipython trainMVAHHbbgg_ptmgg_ptmjj_dR.py
